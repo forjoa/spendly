84b5d1420d054ec47697d22f8372bdabfc9642a8
joa <119975369+forjoa@users.noreply.github.com>
2026-08-11 18:48:10 +0200

    fix: add global styles import and metadata to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from 'next'
+import '../styles/globals.css'
+
+export const metadata: Metadata = {
+  title: 'Spendly',
+  description: 'SaaS for automate expenses',
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
